57c5922e071d7fa008d5cc5bf374a9e62c351e05
Joost Schriek <give@joost.coffee>
2021-03-26 16:12:44 -0400

    adding dryrun mode flag

--- src/Config.cs
+++ src/Config.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace gitman {
     public static class Config
         {
@@ -9,17 +11,24 @@ namespace gitman {
                 public static new string ToString() => $"Github[user={User} token={!string.IsNullOrEmpty(Token)} Org={Org}]";
             }
             
+            private static bool dryrun;
 
             public static string TeamStructureFile { get; set; }
             public static string RepoStructureFile { get; set; }
             public static string ReportingPath { get; set; } = "./";
-            public static bool DryRun { get; set; } = true;
+            public static string DryRunMode { get; set; } = "yes";
+            public static bool DryRun
+            {
+                get { return DryRunMode.Equals("yes"); }
+            }
+            
             public static bool Help { get; set; }
 
             public static bool HasTeamsStructureFile { get => !string.IsNullOrEmpty(TeamStructureFile); }
             public static bool HasRepoStructureFile { get => !string.IsNullOrEmpty(RepoStructureFile); }
 
             public static bool Validate() => !string.IsNullOrEmpty(Github.User) && !string.IsNullOrEmpty(Github.Token)
+                && Regex.IsMatch(DryRunMode, "yes|no")
                 && (HasRepoStructureFile ? System.IO.File.Exists(RepoStructureFile) : true)
                 && (HasTeamsStructureFile ? System.IO.File.Exists(TeamStructureFile) : true);
 
